b72ae9d156f06e04d0510bc3c3db86d0dde5281d
Son Nguyen <hoangson091104@gmail.com>
2025-09-26 22:43:25 -0400

    final: DMBS essentials finished

--- build/basic_test.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE test (id INT, name VARCHAR(50));
-INSERT INTO test VALUES (1, 'Alice');
-INSERT INTO test VALUES (2, 'Bob'), (3, 'Charlie');
-SELECT * FROM test;
-UPDATE test SET name = 'Bobby' WHERE id = 2;
-SELECT * FROM test WHERE id = 2;
-DELETE FROM test WHERE id = 3;
-SELECT * FROM test;
-DROP TABLE test;
